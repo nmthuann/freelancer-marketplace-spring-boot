79a1b979f199272afe7305fa46602255dfab207e
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2024-03-23 21:04:10 +0700

    commit_23_3_feat
    Feat: add H2 database.

--- src/test/java/com/nmt/freelancermarketplacespringboot/repositories/posts/category/CategoryRepositoryTest.java
+++ src/test/java/com/nmt/freelancermarketplacespringboot/repositories/posts/category/CategoryRepositoryTest.java
@@ -4,9 +4,12 @@ import com.nmt.freelancermarketplacespringboot.entities.posts.category.CategoryE
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@DataJpaTest
 class CategoryRepositoryTest {
 
     @Autowired
