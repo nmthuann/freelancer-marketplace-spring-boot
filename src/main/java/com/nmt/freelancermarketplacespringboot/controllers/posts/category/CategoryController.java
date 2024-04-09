8fdee29589ff6fccc9fde487eb1ac4227bb1417d
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2024-04-09 23:55:37 +0700

    commit_9_4_feat
    Feat: learn about Logging.

--- src/main/java/com/nmt/freelancermarketplacespringboot/controllers/posts/category/CategoryController.java
+++ src/main/java/com/nmt/freelancermarketplacespringboot/controllers/posts/category/CategoryController.java
@@ -2,6 +2,7 @@ package com.nmt.freelancermarketplacespringboot.controllers.posts.category;
 
 
 import com.nmt.freelancermarketplacespringboot.common.exceptions.errors.ModuleException;
+import com.nmt.freelancermarketplacespringboot.controllers.posts.post.PostController;
 import com.nmt.freelancermarketplacespringboot.dto.posts.category.CreateCategoryDto;
 import com.nmt.freelancermarketplacespringboot.entities.posts.category.CategoryEntity;
 import com.nmt.freelancermarketplacespringboot.services.posts.category.ICategoryService;
@@ -10,13 +11,20 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -36,6 +44,8 @@ import java.util.List;
 @Tag(name = "Category")
 public class CategoryController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss");
     @Autowired
     ICategoryService categoryService;
 
@@ -99,13 +109,39 @@ public class CategoryController {
 
     @GetMapping("/")
     public ResponseEntity<?> getCategoriesByParentId (
-            @RequestParam int id
-    ) {
+            @RequestParam int id,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
         List<CategoryEntity> result = this.categoryService.getCategoriesByParentId(id);
         if (result.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-        return new ResponseEntity<>(result, HttpStatus.OK);
+//        response.getWriter().println(result);
+//        response.flushBuffer();
+//        int responseSize = response.getBufferSize();
+        LocalDateTime currentTime = LocalDateTime.now();
+        String formattedTime = currentTime.format(formatter);
+        logger.info(String.format("%s - - [%s + 0000] \"%s %s HTTP/1.1\" 200 %s \"%s\" \"%s\"",
+                request.getRemoteAddr(),
+                LocalDateTime.now(),
+                request.getMethod(),
+                request.getRequestURI(),
+                response.getBufferSize(),
+
+                request.getHeader("Referer"),
+                request.getHeader("User-Agent")));
+
+        System.out.println("Log: " +    " " + request.getRemoteAddr() + "- -" +
+                " " + LocalDateTime.now() +
+                " " + request.getMethod() +
+                " " + request.getRequestURI() +
+                " " + 200 +
+                " "  +
+                " " + request.getHeader("User-Agent")
+        );
+        //return new ResponseEntity<>(result, HttpStatus.OK);
+        return ResponseEntity.ok(result);
     }
 
 //    @GetMapping("/all")
